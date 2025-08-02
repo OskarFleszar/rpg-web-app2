0d95b3341db80d6060f6d91507905d03c7a0327b
Oskar Fleszar <mroski2407@gmail.com>
2025-08-02 18:34:41 +0200

    Add the default profile picture

--- rpg-webapp-frontend2/src/pages/ProfilePage.tsx
+++ rpg-webapp-frontend2/src/pages/ProfilePage.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./ProfilePage.css";
+import defaultPfp from "../assets/images/nig.jpg";
 
 export function ProfilePage() {
   const [basicUserData, setBasicUserData] = useState({
@@ -8,6 +9,7 @@ export function ProfilePage() {
     email: "",
     password: "",
   });
+  const [image, setImage] = useState<string | null>(null);
 
   useEffect(() => {
     const loadBasicUserData = async () => {
@@ -23,12 +25,33 @@ export function ProfilePage() {
       );
       setBasicUserData(response.data);
     };
+
+    const fetchProfileImage = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8080/api/user/profileImage",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+            responseType: "blob",
+          }
+        );
+
+        const imageUrl = URL.createObjectURL(response.data);
+        setImage(imageUrl);
+      } catch (error) {
+        console.error("Błąd przy pobieraniu zdjęcia profilowego:", error);
+      }
+    };
+    fetchProfileImage();
     loadBasicUserData();
   }, []);
 
   return (
     <>
       <div className="profile-page">
+        <img className="profile-picture" src={image || defaultPfp} />
         <p>Nickname: {basicUserData.nickname}</p>
         <p>Email: {basicUserData.email}</p>
         <p>Hasło (hash): {basicUserData.password}</p>
